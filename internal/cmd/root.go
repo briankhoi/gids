b4340df8143431ffac97895c3de541cc70b53726
Brian Nguyen <nguyenbriankhoi@gmail.com>
2026-03-03 06:54:31 -0800

    feat: add logger

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,19 +1,32 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
+
+	"gids/internal/logger"
 )
 
 // NewRootCommand returns a fresh root command tree.
 // Using a constructor (rather than a package-level var) lets each test
 // get its own command instance, avoiding flag re-registration panics.
 func NewRootCommand() *cobra.Command {
+	var verbose bool
+
 	root := &cobra.Command{
 		Use:           "gids",
 		Short:         "Git Identity Swap - manage multiple Git identities",
 		SilenceUsage:  true,
 		SilenceErrors: true,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			l := logger.New(os.Stderr, verbose)
+			cmd.SetContext(logger.WithContext(cmd.Context(), l))
+			return nil
+		},
 	}
+
+	root.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "enable debug logging")
 
 	root.AddCommand(newVersionCommand())
 
